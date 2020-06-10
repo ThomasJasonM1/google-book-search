e8c6801ad597416cb9123c10289c5b3d3a465cb5
ThomasJasonM1 <jason@call-em-all.com>
2020-06-10 17:57:06 -0500

    Final updates

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -16,7 +16,7 @@ router
     .route('/search/:term')
     .get((req, res) => {
         console.log(req.params);
-        apiKey = 'AIzaSyDu6Ydp-IBeDkDUDHgvmysXckDh1ofv80Q'
+        apiKey = process.env.GOOGLE_API_KEY;
         const searchterm = req.params.term || 'food';
         axios.get(`https://www.googleapis.com/books/v1/volumes?key=${apiKey}&q=${searchterm}`)
         .then(response => {
